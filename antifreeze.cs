cd94f7ba2869bd06439a795e0a5e6709a84fd239
DUDEX64 <DUDEX64@users.noreply.github.com>
2017-09-24 06:17:25 +1000

    Made and Compiled First Complete Codebase

--- antifreeze.cs
+++ antifreeze.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Threading;
 
 class antifreeze
 {
     static void Main()
     {
-        return; // Code to be written later
+	Process[] apps;
+        while (true)
+	{
+		apps = Process.GetProcesses();
+		foreach (Process app in apps)
+		{
+			if (!app.Responding)
+			{
+				app.Kill();
+				MessageBox.Show("Antifreeze killed " + app.ProcessName + " because it was not responding", "Freeze No More");
+			}
+		}
+		Thread.Sleep(7000);
+	}
     }
 }
